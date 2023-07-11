c3caf142eb8ecc3ab8910483a12ec47a39f79a2a
Michael Marchand <michaeldavidmarchand@gmail.com>
2023-07-11 16:36:10 -0600

    finishes testing basic item state

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,9 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  it 'exists' do
-    item = Item.create!(description: 'the first item', status: 1)
+  let(:item) { Item.create!(description: 'the first item', status: 1) }
 
+  it 'exists' do
     expect(item).to be_an Item
+  end
+
+  it 'has @description and @status' do
+    expect(item.description).to eq('the first item')
+    expect(item.status).to eq(1)
   end
 end
