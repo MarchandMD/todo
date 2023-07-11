942c907c88e264b66f072ed12b919463bd969a9f
Michael Marchand <michaeldavidmarchand@gmail.com>
2023-07-11 16:22:03 -0600

    installs rspec, and configures; 1 successful test

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'exists' do
+    item = Item.create!(description: 'the first item', status: 1)
+
+    expect(item).to be_an Item
+  end
 end
